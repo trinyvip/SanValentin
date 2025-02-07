8de51a3434c33ce22b353e37dda1854bd5dcbade
trinyvip <yostinfarith@gmail.com>
2025-02-07 11:20:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function goToMessagePage() {
+    document.querySelector('.heart').style.display = 'none';
+    document.querySelector('h1').style.display = 'none';
+    document.querySelector('#message-page').classList.add('show');
+}
 
+function showFoodOptions() {
+    document.querySelector('#message-page').classList.remove('show');
+    document.querySelector('#food-options').classList.add('show');
+}
+
+function goToSite(url) {
+    window.location.href = url;
+}
